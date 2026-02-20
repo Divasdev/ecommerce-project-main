5103802990140c7ed98676c06ce6e56645dcc762
Divasdev <sharmadivas881@gmail.com>
2026-02-20 17:59:27 +0530

    lift cart up and share between pages

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,10 +4,10 @@ import './HomePage.css';
 import { Header } from '../components/Header';
 
 
-export function HomePage() {
+export function HomePage({ cart }) {
 
   const [products,setProducts]=useState([]);
-  const[cart,setCart]=useState();
+ 
   useEffect(()=>{
 
   axios.get('/api/products')
@@ -15,10 +15,7 @@ export function HomePage() {
          setProducts(response.data);
       }); 
 
-      axios.get('/api/cart-items')
-         .then((response)=>{
-          setCart(response.data);
-         })
+     
   },[]);
 
 
